2dc7b3ee118cff5973aa4787f676ded460df7cf2
tesssny <teresasnyder@hanovernorwichschools.org>
2016-04-18 14:43:05 -0400

    Updated from Brython Server: 4/18/2016 2:43:05 PM

--- final.py
+++ final.py
@@ -1,1 +1,13 @@
+"""
+sources: http://stackoverflow.com/questions/306400/how-do-i-randomly-select-an-item-from-a-list-using-python, https://inventwithpython.com/chapter9.html, mary feyrer
+"""
 
+import random
+
+words = ['giraffe', 'hangman', 'tulip', 'purple', 'petunia', 'delivery', 'extraneous', 'plywood', 'highway', 'pullover']
+
+newword=random.choice(words)
+
+l=len(newword)
+
+letter1=input(
