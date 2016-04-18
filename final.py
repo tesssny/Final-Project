a1b10f7a5cedaf28a78ff6754a532be124625755
tesssny <teresasnyder@hanovernorwichschools.org>
2016-04-18 14:50:49 -0400

    Updated from Brython Server: 4/18/2016 2:50:49 PM

--- final.py
+++ final.py
@@ -7,13 +7,17 @@ import random
 words = ['giraffe', 'hangman', 'tulip', 'purple', 'petunia', 'delivery', 'extraneous', 'plywood', 'highway', 'pullover']
 
 newword=random.choice(words) #randomly select word
+l=len(newword) #count the number of letters
 
-l=str(len(newword)) #count the number of letters
+word=[] #creating a list of letters in newword
+for x in range(0,l):
+    abc=newword[x]
+    word.append(abc)
+    
+l=str(len(newword)) #convert number of letters to a string so it can be printed
 
 letter1=input("number of spaces="+l+"  ") #first letter guess
 
-l=int(l)
-word=[] #creating a list of letters in newword
 
-for x in range(0,l)
+
     
