4937a9efbf045ff8bcc88eb4746d6f3e72c0fdb4
tesssny <teresasnyder@hanovernorwichschools.org>
2016-04-25 14:22:43 -0400

    Updated from Brython Server: 4/25/2016 2:22:43 PM

--- final.py
+++ final.py
@@ -5,59 +5,66 @@ sources: http://stackoverflow.com/questions/306400/how-do-i-randomly-select-an-i
 import random
 import string
 a=string.ascii_lowercase
-choose=int(input("Type '1' if you want to guess the word. Type '2' if you want the computer to guess your word"))
-words = ['giraffe', 'hangman', 'tulip', 'purple', 'petunia', 'delivery', 'extraneous', 'plywood', 'highway', 'pullover']
+choose=int(input("Type '1' if you want to guess the word. Type '2' if you want the computer to guess your word. "))
 
-newword=random.choice(words) #randomly select word
-l=len(newword) #count the number of letters
-
-word=[] #creating a list of letters in newword
-for x in range(0,l):
-    abc=newword[x]
-    word.append(abc)
-
-letter1=input("number of spaces = "+str(l)+". After five wrong guesses, you lose. First guess? ") #first letter guess
-while letter1 not in a:
-    letter1=input("Invalid input. Please guess a lowercase letter.") 
-
-guess=[]
-turns=0
-num=0
-
-while num<1:
-    for x in range(0,l):
-        if letter1==word[x]:
-            guess.append(letter1)
-        else:
-            guess.append('_')
-        if letter1 not in word:
-            turns=turns+1
-    num=num+1
-
-while turns<5 and num>0:
+if choose!=1 and choose!=2:
+    print("Invalid input. Start over.")
     
+if choose==1:
+    words = ['giraffe', 'hangman', 'tulip', 'purple', 'petunia', 'delivery', 'extraneous', 'plywood', 'highway', 'pullover']
+    
+    newword=random.choice(words) #randomly select word
+    l=len(newword) #count the number of letters
+    
+    word=[] #creating a list of letters in newword
+    for x in range(0,l):
+        abc=newword[x]
+        word.append(abc)
+    
+    letter1=input("number of spaces = "+str(l)+". After five wrong guesses, you lose. First guess? ") #first letter guess
     while letter1 not in a:
         letter1=input("Invalid input. Please guess a lowercase letter.") 
     
-    letter1=input(' '.join(guess)+" Number of wrong guesses = "+str(turns)+". Next guess?")
+    guess=[]
+    turns=0
+    num=0
     
+    while num<1:
+        for x in range(0,l):
+            if letter1==word[x]:
+                guess.append(letter1)
+            else:
+                guess.append('_')
+            if letter1 not in word:
+                turns=turns+1
+        num=num+1
     
-    for x in range(0,l):
-        if letter1==word[x]:
-            guess[x]=letter1
-        elif guess[x] not in a:
-            guess[x]='_'
-    
-    if letter1 not in word:
-        turns=turns+1
-    
-    
-    if turns==5:
-        print("Game Over")
-    
-    if word==guess:
-        print("The word is "+newword+". You won!")
-        turns=5
+    while turns<5 and num>0:
+        
+        while letter1 not in a:
+            letter1=input("Invalid input. Please guess a lowercase letter.") 
+        
+        letter1=input(' '.join(guess)+" Number of wrong guesses = "+str(turns)+". Next guess?")
+        
+        
+        for x in range(0,l):
+            if letter1==word[x]:
+                guess[x]=letter1
+            elif guess[x] not in a:
+                guess[x]='_'
+        
+        if letter1 not in word:
+            turns=turns+1
+        
+        
+        if turns==5:
+            print("Game Over")
+        
+        if word==guess:
+            print("The word is "+newword+". You won!")
+            turns=5
 
 
+if choose==2:
+    print("cool")
     
